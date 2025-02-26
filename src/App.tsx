623d248ce3125d4f5b4ca105c078caee52644114
sou <sou.suke.i0318@gmail.com>
2025-02-26 21:22:04 +0900

    クリック時に色が変わるように

--- src/App.tsx
+++ src/App.tsx
@@ -20,9 +20,9 @@ export default function App() {
                 result.push(
                     <details className={[css.title, css.hover].join(" ")} key={i} tabIndex={-1}>
                         <summary>{v.title}</summary>
-                        {!v.username || <div className={css.hover} title="Click To Copy" onClick={() => copy(v.username)}>UserName</div>}
-                        {!v.mail     || <div className={css.hover} title="Click To Copy" onClick={() => copy(v.mail)}>Mail Address</div>}
-                        {!v.password || <div className={css.hover} title="Click To Copy" onClick={() => copy(v.password)}>Password</div>}
+                        {!v.username || <div className={[css.hover, css.click].join(" ")} title="Click To Copy" onClick={() => copy(v.username)}>UserName</div>}
+                        {!v.mail     || <div className={[css.hover, css.click].join(" ")} title="Click To Copy" onClick={() => copy(v.mail)}>Mail Address</div>}
+                        {!v.password || <div className={[css.hover, css.click].join(" ")} title="Click To Copy" onClick={() => copy(v.password)}>Password</div>}
                         <div className={css.note}>{v.note}</div>
                     </details>
                 );
