61a234c8b155be985189f7db92ac3bc42241d894
sou <sou.suke.i0318@gmail.com>
2025-02-26 20:24:08 +0900

    検索機能実装(startswith)

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ import css from "./App.module.css";
 
 export default function App() {
     const [view, setView] = useState<React.JSX.Element[]>([]);
+    const [search, setSearch] = useState("");
 
     useEffect(() => {
         const result: React.JSX.Element[] = [];
         dataload().then(data => {
             data.forEach((v, i) => {
+                if (search != "" && !v.title.toLowerCase().startsWith(search.toLowerCase())) return;
                 result.push(
                     <details className={[css.title, css.hover].join(" ")} key={i}>
                         <summary>{v.title}</summary>
@@ -21,12 +23,12 @@ export default function App() {
             });
             setView(result);
         });
-    }, []);
+    }, [search]);
 
     return (
         <>
             <div className={css.close}>CLOSE</div>
-            <input className={css.search} type="text" placeholder="search"/>
+            <input className={css.search} onChange={e=>setSearch(e.currentTarget.value)} type="text" placeholder="search"/>
             <div className={css.main}>{view}</div>
         </>
     );
