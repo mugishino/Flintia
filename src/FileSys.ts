caff67057aea3ab16114c7443b44e1e052ae462b
sou <sou.suke.i0318@gmail.com>
2025-02-26 12:38:54 +0900

    セーブデータ用クラスを用意

---
+++ src/FileSys.ts
@@ -0,0 +1,35 @@
+import { appDataDir } from "@tauri-apps/api/path";
+import { exists, mkdir, readTextFile, writeTextFile } from "@tauri-apps/plugin-fs";
+
+export class SaveData {
+    title       = "";
+    username    = "";
+    mail        = "";
+    password    = "";
+    note        = "";
+}
+
+async function getSaveFile() {
+    const dir = await appDataDir();
+    if (!(await exists(dir))) mkdir(dir);
+    return dir;
+}
+
+export default class FileSys {
+    public static async save(data: SaveData[]) {
+        const json = JSON.stringify(data);
+        await writeTextFile(await getSaveFile(), json);
+    }
+
+    public static async load() {
+        const path = await getSaveFile();
+        if (!(await exists(path))) {
+            return [];
+        }
+
+        const raw = await readTextFile(await getSaveFile());
+        const json: Object[] = JSON.parse(raw);
+        const result: SaveData[] = json.map(v => Object.assign(new SaveData(), v));
+        return result;
+    }
+}
