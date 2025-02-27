13895f24cd80d81533e609c28627202db4ed2f04
sou <sou.suke.i0318@gmail.com>
2025-02-27 23:26:48 +0900

    非表示を表示するボタン

--- src/App.tsx
+++ src/App.tsx
@@ -12,16 +12,19 @@ export default function App() {
     const [view, setView] = useState<React.JSX.Element[]>([]);
     const [search, setSearch] = useState("");
 
+    // setting
+    const [showHide, setShowHide] = useState(false);
+
     useEffect(() => {
         const result: React.JSX.Element[] = [];
         dataload().then(data => {
             data.sort((a, b) => a.title.localeCompare(b.title)) // A-Zでソート
             .forEach((v, i) => {
-                if (v.hide) return;
+                if (v.hide && !showHide) return;
                 if (search != "" && !v.title.toLowerCase().startsWith(search.toLowerCase())) return;
                 result.push(
                     <details className={[css.title, css.hover].join(" ")} key={i} tabIndex={-1}>
-                        <summary>{v.title}</summary>
+                        <summary className={v.hide ? css.showHide : undefined}>{v.title}</summary>
                         {!v.username || <div className={[css.hover, css.click].join(" ")} title="Click To Copy" onClick={() => copy(v.username)}>UserName</div>}
                         {!v.mail     || <div className={[css.hover, css.click].join(" ")} title="Click To Copy" onClick={() => copy(v.mail)}>Mail Address</div>}
                         {!v.password || <div className={[css.hover, css.click].join(" ")} title="Click To Copy" onClick={() => copy(v.password)}>Password</div>}
@@ -31,12 +34,15 @@ export default function App() {
             });
             setView(result);
         });
-    }, [search]);
+    }, [search, showHide]);
 
     return (
         <>
             <input autoFocus className={css.search} onChange={e=>setSearch(e.currentTarget.value)} type="text" placeholder="search"/>
             <div className={css.main}>{view}</div>
+            <div className={css.setting}>
+                <button onClick={() => setShowHide(!showHide)} className={showHide ? css.enable : undefined}>ShowHide</button>
+            </div>
         </>
     );
 }
