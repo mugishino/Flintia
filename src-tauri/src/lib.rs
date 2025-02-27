4b4512b24ef137a54e70b97968e0400dae30fb9a
sou <sou.suke.i0318@gmail.com>
2025-02-28 08:51:51 +0900

    tauri add dialog

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -21,8 +21,12 @@ fn hide(win: tauri::WebviewWindow) {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_single_instance::init(|app, _, _| {
-            let _ = app.get_webview_window("main").expect("no main window").set_focus();
+            let _ = app
+                .get_webview_window("main")
+                .expect("no main window")
+                .set_focus();
         }))
         .plugin(tauri_plugin_autostart::init(
             MacosLauncher::LaunchAgent,
