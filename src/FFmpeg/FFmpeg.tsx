fc78c43ef158fcc44aeebc239b0409f2bbcea9bb
sou <sou.suke.i0318@gmail.com>
2025-04-28 14:44:15 +0900

    仮完成、一旦動きます

--- src/FFmpeg/FFmpeg.tsx
+++ src/FFmpeg/FFmpeg.tsx
@@ -34,13 +34,6 @@ enum QualityMode {
     CQVBR = "cq", // 品質優先可変ビットレート
 }
 
-enum Extension {
-    mp4 = "mp4",
-    mkv = "mkv",
-    webm = "webm",
-    mov = "mov",
-}
-
 function BuildCommand(
     i: string,
     videoCodec: VideoCodec,
@@ -49,7 +42,6 @@ function BuildCommand(
     qualityMode: QualityMode,
     qualityValue: number,
     outputFileName: string,
-    outputFileExtension: Extension,
 ) {
     let command = ["ffmpeg"];
     command.push("-i", i); // input file
@@ -59,23 +51,24 @@ function BuildCommand(
 
     // quality mode
     command.push("-rc", qualityMode);
+    // qualityValue = 0 でビットレート固定を無効化
     switch (qualityMode) {
         case QualityMode.CQP:
             command.push("-qp", qualityValue.toString());
-            break;
-        case QualityMode.CBR:
-            command.push("-b:v", qualityValue+"K");
+            qualityValue = 0;
             break;
         case QualityMode.CQVBR:
             command.push("-cq", qualityValue.toString());
+            qualityValue = 0;
             break;
     }
+    command.push("-b:v", qualityValue+"K");
 
     // その他
     command.push("-movflags", "+faststart"); // メタ情報を先頭に配置
 
     // output file
-    command.push(outputFileName+"."+outputFileExtension);
+    command.push(outputFileName);
     return command.join(" ");
 }
 
@@ -94,7 +87,7 @@ function EnumToOptions<T extends object>(arg: T) {
     return elems;
 }
 
-// TODO: Extensionを直す
+// TODO: Extensionを直す mp4, mkv, mov, webm
 // TODO: QualityValueを作る
 // TODO: セレクタのコンポーネント化
 
@@ -105,8 +98,7 @@ export default function FFmpeg() {
     const [audioCodec, setAudioCodec] = useState(AudioCodec.aac);
     const [qualityMode, setQualityMode] = useState(QualityMode.CQP);
     const [qualityValue, setQualityValue] = useState(0);
-    const [outputFile, setOutputFile] = useState<string>("output.webm");
-    const [extension, setExtension] = useState(Extension.webm);
+    const [outputFile, setOutputFile] = useState<string|null>(null);
 
     return (
         <>
@@ -155,42 +147,39 @@ export default function FFmpeg() {
                 </select>
             </div>
             <div className={css.setting}>
+                <span>{qualityMode == QualityMode.CBR ? "Bitrate" : "QualityLevel"}</span>
+                <input className={css.button} type="number"
+                    defaultValue={qualityMode == QualityMode.CBR ? 4096 : 20}
+                    step={qualityMode == QualityMode.CBR ? 1024 : 1}
+                    min={qualityMode == QualityMode.CBR ? 1024 : 15}
+                    max={qualityMode == QualityMode.CBR ? 65536 : 28}
+                    onChange={v => setQualityValue(v.target.valueAsNumber)}/>
+            </div>
+            <div className={css.setting}>
                 <span>OutputFile</span>
                 <button className={css.button} onClick={() => {
                     save({
-                        filters: [{
-                            name: "webm",
-                            extensions: ["webm"]
-                        }],
+                        filters: [
+                            {name: "", extensions: ["webm"]},
+                            {name: "", extensions: ["mkv" ]},
+                            {name: "", extensions: ["mp4" ]},
+                            {name: "", extensions: ["mov" ]},
+                        ],
                         title: "Output file",
                     }).then(f => {
                         if (f != null) setOutputFile(f);
                         WInvoke.show();
                     });
-                }}>{outputFile.split("\\").slice(-1)[0] ?? "Browse..."}</button>
-            </div>
-            <div className={css.setting}>
-                <span>OutputExtension</span>
-                <select className={css.selector} defaultValue={extension} onChange={v => setExtension(v.target.value as Extension)}>
-                    {EnumToOptions(Extension)}
-                </select>
+                }}>{outputFile?.split("\\").slice(-1)[0] ?? "Browse..."}</button>
             </div>
             <button style={{fontSize: "1.2rem"}} className={css.button} disabled={(() => {
                 // ボタン無効化条件
                 if (inputFile == null) return true;
+                if (outputFile == null) return true;
                 return false;
             })()} onClick={() => {
-                const cmd = BuildCommand(
-                    // @ts-ignore
-                    inputFile,
-                    videoCodec,
-                    preset,
-                    audioCodec,
-                    qualityMode,
-                    0,
-                    outputFile,
-                    extension
-                );
+                // @ts-ignore
+                const cmd = BuildCommand(inputFile, videoCodec, preset, audioCodec, qualityMode, 0, outputFile);
                 navigator.clipboard.writeText(cmd);
             }}>Copy FFmpeg command</button>
         </>
