932b6e42debe51c0e63ea24a42363f135d72cedf
sou <sou.suke.i0318@gmail.com>
2025-05-15 17:34:56 +0900

    Toolsから通知欄を削除

--- src/Tools/Tools.tsx
+++ src/Tools/Tools.tsx
@@ -2,23 +2,15 @@ import { useState } from "react";
 import css from "./Tools.module.css";
 
 export default function Tools() {
-    const [notify, setNotify] = useState("");
-    function showNotification(msg: string) {
-        setNotify(msg);
-        setTimeout(setNotify, 3000);
-    }
-
     const [uuidView, setUUID] = useState(crypto.randomUUID());
 
 
     function copyText(text: string) {
         navigator.clipboard.writeText(text);
-        showNotification("Copied!")
     }
 
     return (
         <>
-            <div className={css.notifis}>{notify}</div>
             <div className={css.tool}>
                 <div className={css.title}>UUID Generator</div>
                 {(() => {
