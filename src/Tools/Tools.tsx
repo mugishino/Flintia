d8f548739386b3c582a73d142f4ace969ede2fa5
sou <sou.suke.i0318@gmail.com>
2025-05-15 18:20:34 +0900

    MIN_MAX_VALUEを追加

--- src/Tools/Tools.tsx
+++ src/Tools/Tools.tsx
@@ -24,6 +24,32 @@ export default function Tools() {
                     >{uuidView}</div>
                 })()}
             </div>
+
+            <div className={css.tool}>
+                <div className={css.title}>MIN_MAX_VALUE</div>
+                {(() => {
+                    function CreateElem(props: {title: string, min: number, max: number}) {
+                        return <div
+                            className={css.button+" inline-block w-38"}
+                            title="LeftClick: MAX, RightClick: MIN"
+                            onClick   ={() => copyText(props.max.toString())}
+                            onAuxClick={() => copyText(props.min.toString())}
+                        >{props.title}</div>;
+                    }
+                    return (
+                        <div>
+                            <CreateElem title="sbyte"  min={-128}                 max={127}/>
+                            <CreateElem title="byte"   min={0}                    max={255}/>
+                            <CreateElem title="ushort" min={0}                    max={65535}/>
+                            <CreateElem title="short"  min={-32768}               max={32767}/>
+                            <CreateElem title="uint"   min={0}                    max={4294967295}/>
+                            <CreateElem title="int"    min={-2147483648}          max={2147483647}/>
+                            <CreateElem title="ulong"  min={0}                    max={18446744073709551615}/>
+                            <CreateElem title="long"   min={-9223372036854775808} max={9223372036854775807}/>
+                        </div>
+                    );
+                })()}
+            </div>
         </>
     );
 }
