d5411223ef519a10bac248f044ea416b82c2942f
sou <sou.suke.i0318@gmail.com>
2025-06-16 14:48:20 +0900

    Buttonをクリックしたときの色を変えれるように

--- src/Components.tsx
+++ src/Components.tsx
@@ -15,14 +15,21 @@ export function Button(props: {
     onClick     ?: (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => void,
     onAuxClick  ?: (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => void,
     onMouseDown ?: (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => void,
+    clickColor  ?: "green"|"red"|"blue",
     children: React.ReactElement|string,
 }) {
+    const clickcolor = {
+        "red": "bg-red-800",
+        "blue": "bg-blue-800",
+        "green": "bg-green-800",
+    }[props.clickColor ?? "green"];
+
     return (
         <div
         className={cls(
             "inline-block cursor-pointer text-center text-white border-1 border-layerB bg-layerB transition-[200ms]",
             "hover:bg-layerC",
-            "active:transition-[0ms] active:bg-green-800",
+            `active:transition-[0ms] active:${clickcolor}`,
             props.className,
         )}
         title={props.title} onClick={props.onClick} onAuxClick={props.onAuxClick} onMouseDown={props.onMouseDown}
