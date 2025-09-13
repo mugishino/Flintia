b0a6c013123a8421af91896e526a45381dcf578f
sou <sou.suke.i0318@gmail.com>
2025-09-13 12:50:09 +0900

    QRコード生成を追加

--- src/QRCode/QRCode.tsx
+++ src/QRCode/QRCode.tsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { BrowserQRCodeReader } from "@zxing/browser";
 import jsQR from "jsqr";
 import { getClipboardImageBlob } from "~/util";
+import { EvenlyDividedRow } from "~/Components";
+import * as mkqr from "qrcode";
 
 async function tryZxing(blob: Blob) {
     const url = URL.createObjectURL(blob);
@@ -33,21 +35,38 @@ export default function QRCode() {
     const [result, setResult] = useState<string|null>(null);
     const [success, setSuccess] = useState(true);
 
+    async function readFromClipboard() {
+        setSuccess(false);
+        const blob = await getClipboardImageBlob();
+        if (blob == null) return setResult("クリップボードが画像ではありません");
+
+        const qrdata = await tryZxing(blob) ?? await tryJsqr(blob);
+        if (qrdata == null) return setResult("QRコードを読み取れませんでした");
+        setResult(qrdata);
+        setSuccess(true);
+    }
+
+    async function createFromClipboard() {
+        setSuccess(false);
+        const canvas = document.createElement("canvas");
+        const text = await navigator.clipboard.readText();
+        if (text.length == 0) return setResult("QRコード生成には文字列が必要です");
+
+        mkqr.toCanvas(canvas, text);
+        canvas.toBlob(blob => {
+            if (blob == null) return;
+            navigator.clipboard.write([new ClipboardItem({[blob.type]: blob})]);
+            setResult("生成したQRコードをコピーしました");
+            setSuccess(true);
+        });
+    }
+
     return (
         <>
-            <button onClick={async () => {
-                setSuccess(false);
-                const blob = await getClipboardImageBlob();
-                if (blob == null) return setResult("クリップボードが画像ではありません");
-
-                const qrdata = await tryZxing(blob) ?? await tryJsqr(blob);
-                if (qrdata == null) {
-                    setResult("QRコードを読み取れませんでした");
-                } else {
-                    setResult(qrdata);
-                    setSuccess(true);
-                }
-            }} className="bg-layerB border-0 border-b-1 hover:bg-layerC cursor-pointer">Read from clipboard</button>
+            <EvenlyDividedRow>
+                <button onClick={readFromClipboard} className="border-0 border-b-1">Read from clipboard</button>
+                <button onClick={createFromClipboard} className="border-0 border-b-1 border-l-1">Create from clipboard</button>
+            </EvenlyDividedRow>
             <div className={`grow text-center select-text break-words ${success ? "text-white" : "text-fail"}`}>{(() => {
                 if (result == null) return null;
                 if (URL.canParse(result)) {
