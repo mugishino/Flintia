b2683fd003cec8c9f0b5526573ee1683da566524
sou <sou.suke.i0318@gmail.com>
2025-09-30 20:03:06 +0900

    404ページのホームへ戻るボタンをbuttonからLinkに変更

--- src/404.tsx
+++ src/404.tsx
@@ -1,14 +1,13 @@
-import { useNavigate } from "react-router";
+import { Link } from "react-router";
 
 export default function NotFoundPage() {
-    const navigate = useNavigate();
     return (
         <div className="bg-[#0077D6] grow flex flex-col p-8 text-2xl">
             <span className="text-8xl pb-8">:(</span>
             <span>The page you are trying to access cannot be found.</span>
             <span>This appears to be due to a bug.</span>
             <span className="py-4">404% Complete</span>
-            <span className="cursor-pointer underline" onClick={() => navigate("/")}>{">Return Home<"}</span>
+            <Link className="underline" to="/">{">Return Home<"}</Link>
         </div>
     );
 }
