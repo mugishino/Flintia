0418aefb2e78564cc6ce6a0ab5e18c4fa51b8259
sou <sou.suke.i0318@gmail.com>
2025-09-30 17:29:13 +0900

    InvokeWrapperをclassから変更

--- src/InvokeWrapper.ts
+++ src/InvokeWrapper.ts
@@ -1,24 +1,24 @@
 import { invoke } from "@tauri-apps/api/core";
 
-export class WInvoke {
-    public static async show() {
+export const WInvoke = {
+    async show() {
         await invoke("show");
-    }
+    },
 
-    public static async hide() {
+    async hide() {
         await invoke("hide");
-    }
+    },
 
-    public static async paste() {
+    async paste() {
         await invoke("paste");
-    }
+    },
 
     // 互換性維持のために残してください
-    public static async openExplorer(path: string) {
+    async openExplorer(path: string) {
         await this.runProcess("explorer", path);
-    }
+    },
 
-    public static async runProcess(file: string, ...args: string[]) {
+    async runProcess(file: string, ...args: string[]) {
         await invoke("run_process", {file: file, args: args});
-    }
+    },
 }
