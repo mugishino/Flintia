a91f149d4ef1231284ac40cf1f0e112eb7ea97c4
sou <sou.suke.i0318@gmail.com>
2025-10-09 06:55:30 +0900

    UnixToTimeのタイムゾーンを選択式に

--- src/global/number.ts
+++ src/global/number.ts
@@ -10,6 +10,11 @@ declare global {
          * 数値がNaNだった場合にデフォルトの値を返します。
          */
         orDefault(default_: number): number;
+        /**
+         * 数値を文字列にゼロ埋めして変換します。
+         * @param includeMinus マイナスを文字数に含むか
+         */
+        toStringZero(size: number, includeMinus?: boolean): string;
     }
 }
 
@@ -17,6 +22,19 @@ Number.prototype.toInt = function() {
     return Math.trunc(this.valueOf());
 }
 
-Number.prototype.orDefault = function(default_: number) {
-    return isNaN(this.valueOf()) ? default_ : this.valueOf();
+Number.prototype.orDefault = function(default_) {
+    const num = this.valueOf()
+    return isNaN(num) ? default_ : num;
+}
+
+Number.prototype.toStringZero = function(size, includeMinus=false) {
+    const num = this.valueOf();
+    const minus = num < 0;
+    const abs = Math.abs(num).toString();
+    const numLength = abs.length;
+    return (
+        (minus?"-":String.empty)
+        + "0".repeat(size - numLength - (includeMinus&&minus?1:0))
+        + abs
+    );
 }
--- src/pages/Tools/UnixToTime.tsx
+++ src/pages/Tools/UnixToTime.tsx
@@ -17,7 +17,7 @@ export default function UnixToTime() {
     const text =
     `${date.getUTCFullYear()}年${date.getUTCMonth()+1}月${date.getUTCDate()}日 `+
     `${date.getUTCHours()}時${date.getUTCMinutes()}分${date.getUTCSeconds()}秒`+
-    `${date.getUTCMilliseconds()}${zone>=0?"+":String.empty}${zone}00`;
+    `${date.getUTCMilliseconds()} UTC${zone>=0?"+":String.empty}${zone}`;
 
 
 
@@ -25,11 +25,40 @@ export default function UnixToTime() {
         <div className="flex flex-col">
             <div className="flex flex-row">
                 <input type="number" className="w-full" placeholder="UnixTime(ms)" value={time} onChange={e => setTime(e.currentTarget.valueAsNumber.orDefault(0))}/>
-                <div className="flex flex-row">
-                    <button className="w-10" onClick={() => setZone(zone-1)}>{"<"}</button>
-                    <input type="number" placeholder="TimeZone" value={zone} onChange={e => setZone(e.currentTarget.valueAsNumber.orDefault(0))}/>
-                    <button className="w-10" onClick={() => setZone(zone+1)}>{">"}</button>
-                </div>
+                <select value={zone} onChange={v => setZone(parseInt(v.currentTarget.value))}>{
+                    [
+                        "UTC+12より1日遅い",
+                        "ミッドウェー島, UTC+13より1日遅い",
+                        "ハワイ",
+                        "アラスカ",
+                        "太平洋標準時(米, カナダ), バハカリフォルニア",
+                        "山岳標準時(米, カナダ), アリゾナ",
+                        "中部標準時(米, カナダ), 中央メキシコ",
+                        "東部標準時(米, カナダ), ニューヨーク",
+                        "ニューヨーク(夏時間), チリ",
+                        "アルゼンチン",
+                        "ブラジル(フェルナンド・デ・ノローニャ)",
+                        "アゾレス諸島, カーボベルデ",
+                        "協定世界時, イギリス", // UTC+0000
+                        "アムステルダム",
+                        "ウクライナ, ギリシャ",
+                        "モスクワ, サウジアラビア, ベラルーシ",
+                        "アゼルバイジャン, アラブ首長国連邦",
+                        "パキスタン",
+                        "カザフスタン, バングラデシュ",
+                        "タイ, ベトナム, インドネシア西部",
+                        "香港, 北京",
+                        "東京, 韓国, パラオ",
+                        "グアム",
+                        "ソロモン諸島",
+                        "ニュージーランド",
+                        "トンガ, サモア, UTC-11より1日早い",
+                        "キリバス(ライン諸島), UTC-10より1日早い"
+                    ].map((v, i) => {
+                        const t = i-12;
+                        return <option key={t+v} value={t}>UTC{t>=0?"+":String.empty}{t.toStringZero(2)} {v}</option>
+                    })
+                }</select>
             </div>
             <button onClick={() => {
                 Clipboards.copyText(text);
