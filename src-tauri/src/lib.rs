ee25342f613eabd542dbd12c1295ecfdb537e0a9
sou <sou.suke.i0318@gmail.com>
2025-12-31 04:59:57 +0900

    Add notification plugin

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -10,6 +10,7 @@ mod commands;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_notification::init())
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_single_instance::init(|app, _, _| {
