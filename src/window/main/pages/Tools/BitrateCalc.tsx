1e93f3a9b1aecab14ade6e5add2c30bcf4a63da4
mugishino <sou.suke.i0318@gmail.com>
2026-04-29 04:34:50 +0900

    Tools/BitrateCalc: CtrlやShiftでStepを変えれるように #10

--- src/window/main/pages/Tools/BitrateCalc.tsx
+++ src/window/main/pages/Tools/BitrateCalc.tsx
@@ -8,7 +8,18 @@ export function BitrateCalc(props: {
     const [targetSize, setTargetSize] = useState(10);
     const [audioBitrate, setAudioBitrate] = useState(192);
 
+    const [durationFocus, setDurationFocus] = useState(false);
+
     const duration = props.duration ?? stateDuration;
+
+    // render
+    const hour = Math.floor(duration / 3600);
+    const minute = Math.floor((duration % 3600) / 60);
+    const second = duration % 60;
+    const timeText: string[] = [];
+    if (hour   > 0) timeText.push(`${hour}時間`);
+    if (minute > 0) timeText.push(`${minute}分`);
+    if (second > 0) timeText.push(`${second}秒`);
 
     // 映像Kbps = (目標MiB容量 x 8192 / 秒数) - 音声Kbps
     const videoBitrate = (targetSize * 8192 / duration) - audioBitrate;
@@ -16,7 +27,24 @@ export function BitrateCalc(props: {
     return (
         <>
             <Setting title="動画長(秒)">
-                <input type="number" disabled={!!props.duration} min={0} value={props.duration ?? duration} onChange={v => setDuration(v.currentTarget.valueAsNumber.orDefault(0))}/>
+                <input
+                    type={durationFocus ? "number" : "text"}
+                    disabled={!!props.duration}
+                    min={0}
+                    value={durationFocus ? (props.duration ?? duration) : timeText.join(String.space)}
+                    onChange={v => setDuration(v.currentTarget.valueAsNumber.orDefault(0))}
+                    onFocus={() => setDurationFocus(true)}
+                    onBlur={() => setDurationFocus(false)}
+                    onKeyDown={e => {
+                        const key = e.key.toLowerCase();
+                        if (key == "arrowdown" || key == "arrowup") {
+                            const num = key == "arrowup" ? 1 : -1;
+                            const step = e.shiftKey ? 60*60 : e.ctrlKey ? 60 : 1;
+                            setDuration(v => (v + (num * step)).keepRange(0, Infinity));
+                            e.preventDefault();
+                        }
+                    }
+                }/>
             </Setting>
             <Setting title="目標容量(MB)">
                 <input type="number" min={0} value={targetSize} onChange={v => setTargetSize(v.currentTarget.valueAsNumber.orDefault(0))}/>
